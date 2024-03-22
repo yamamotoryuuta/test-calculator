c77935f18dd2da04f2f4f99ad167bbf4744fe204
山本 龍太 ゲスト <yamamoto_ryuta@external.cygames.jp>
2024-03-23 07:11:28 +0900

    表示修正

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,86 +1,103 @@
-import { useState} from 'react';
-import '../styles/App.css';
+import { useState } from "react";
+import "../styles/App.css";
 
 function App() {
-  const [display, setDisplay] = useState<string>("0");
-  const [firstNum, setFirstNum] = useState<number>(0);
-  const [secondNum, setSecondNum] = useState<number>(0);
-  const [operator, setOperator] = useState<string>("");
+	const [display, setDisplay] = useState<string>("0");
+	const [firstNum, setFirstNum] = useState<number>(0);
+	const [secondNum, setSecondNum] = useState<number>(0);
+	const [operator, setOperator] = useState<string>("");
 
+	const resetHndler = () => {
+		setFirstNum(0);
+		setSecondNum(0);
+		setOperator("");
+		setDisplay("0");
+	};
+	const setFirstNumHandler = (num: number) => {
+		setFirstNum(num);
+	};
+	const setSecondNumHandler = (num: number) => {
+		setSecondNum(num);
+	};
+	const setOperatorHandler = (op: string) => {
+		setOperator(op);
+	};
 
-  const resetHndler = () => {
-    setFirstNum(0);
-    setSecondNum(0);
-    setOperator("");
-    setDisplay("0");
-  }
-  const setFirstNumHandler = (num: number) => { setFirstNum(num); };
-  const setSecondNumHandler = (num: number) => { setSecondNum(num); };
-  const setOperatorHandler = (op: string) => { setOperator(op); };
+	const setNumber = (num: number) => {
+		const current_num = operator === "" ? firstNum : secondNum;
 
-  const setNumber = (num: number) => {
-    const current_num = operator === "" ? firstNum : secondNum;
+		let push_num = 0;
+		if (num === 0 && current_num === 0) {
+			return;
+		} else if (num === 0 && current_num !== 0) {
+			push_num = current_num * 10;
+		} else {
+			push_num = current_num * 10 + num;
+		}
 
-    let push_num = 0;
-    if (num === 0 && current_num === 0) {
-      return;
-    } else if (num === 0 && current_num !== 0) {
-      push_num = current_num * 10;
-    } else {
-      push_num = current_num * 10 + num;
-    }
+		if (operator === "") {
+			setFirstNumHandler(push_num);
+			setDisplay(push_num.toString());
+		} else {
+			setSecondNumHandler(push_num);
+			setDisplay(push_num.toString());
+		}
+	};
 
-    if (operator === "") {
-      setFirstNumHandler(push_num);
-    } else {
-      setSecondNumHandler(push_num);
-    }
-  }
+	return (
+		<div className="App">
+			<p>
+				合計: {display} <span>{operator}</span>
+			</p>
+			<button onClick={resetHndler}> C・CE</button>
 
-  return (
-    <div className="App">
-      <p>合計: {display} <span>{ operator}</span></p>
-      <button onClick={resetHndler}> C・CE</button>
+			{[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => {
+				return (
+					<button
+						key={num}
+						onClick={() => {
+							setNumber(num);
+						}}
+					>
+						{num}
+					</button>
+				);
+			})}
 
-      {
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => {
-          return (
-            <button key={num} onClick={() => {
-              setNumber(num);
-              setDisplay(num.toString());
-            }
-            }>{num}</button>
-          );
-        })
-      }
+			{["+", "-", "*", "/"].map((op) => {
+				return (
+					<button
+						key={op}
+						onClick={() => {
+							setOperatorHandler(op);
+							// setDisplay(op);
+						}}
+					>
+						{op}
+					</button>
+				);
+			})}
 
-      {
-        ["+", "-", "*", "/"].map((op) => {
-          return (
-            <button key={op} onClick={() => {
-              setOperatorHandler(op);
-              // setDisplay(op);
-            }}>{op}</button>
-          );
-        })
-      }
-
-      <button onClick={() => {
-        let answer = 0;
-        if (operator === "+") {
-          answer = firstNum + secondNum;
-        } else if (operator === "-") {
-          answer = firstNum - secondNum;
-        } else if (operator === "*") {
-          answer = firstNum * secondNum;
-        } else if (operator === "/") {
-          answer = firstNum / secondNum;
-        }
-        setDisplay(answer.toString());
-        setFirstNumHandler(answer);
-      }}>=</button>
-    </div>
-  );
+			<button
+				onClick={() => {
+					let answer = 0;
+					if (operator === "+") {
+						answer = firstNum + secondNum;
+					} else if (operator === "-") {
+						answer = firstNum - secondNum;
+					} else if (operator === "*") {
+						answer = firstNum * secondNum;
+					} else if (operator === "/") {
+						answer = firstNum / secondNum;
+					}
+					setDisplay(answer.toString());
+					setFirstNumHandler(answer);
+				}}
+			>
+				=
+			</button>
+		</div>
+	);
 }
 
 export default App;
