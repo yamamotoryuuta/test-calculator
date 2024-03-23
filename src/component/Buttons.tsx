6f07142a01147c997722043e026e93675e899f35
山本 龍太 ゲスト <yamamoto_ryuta@external.cygames.jp>
2024-03-23 14:25:45 +0900

    floor修正

--- src/component/Buttons.tsx
+++ src/component/Buttons.tsx
@@ -13,13 +13,22 @@ function Buttons({
     calcs: Calcs;
     setCalcs: (calcs: Calcs) => void;
   }) {
-    const [autoFlg, setAutoFlg] = useState(false);
+  const [autoFlg, setAutoFlg] = useState(false);
+  const [floorFlg, setFloorFlg] = useState(false);
 
+  /**
+   * リセットボタンの処理：表示を0にする　cボタン
+   */
 	const resetHandler = () => {
-		setDisplay("0");
+    setDisplay("0");
+    setAutoFlg(false);
+    setFloorFlg(false);
     setCalcs({firstNum: 0, secondNum: 0, operator: ""});
   };
 
+  /**
+   * 計算ボタンの処理：計算結果を表示する　=ボタン
+   */
   const calcHandler = () => {
     const answer = calcAnswer(calcs.firstNum, calcs.secondNum, calcs.operator as Operator);
     setDisplay(answer.toString());
@@ -27,7 +36,10 @@ function Buttons({
     setCalcs({ firstNum: answer, secondNum: calcs.secondNum, operator: calcs.operator});
   }
 
-
+  /**
+   * 演算子ボタンの処理：演算子をセットする
+   * @param op 演算子
+   */
   const optHandler = (op: Operator) => {
     if (calcs.operator !== "" && calcs.secondNum !== 0) {
       const answer = calcAnswer(calcs.firstNum, calcs.secondNum, calcs.operator as Operator);
@@ -39,42 +51,60 @@ function Buttons({
     }
   }
 
-  const numBtnHandler = (num: number | string) => {
+  const numBtnHandler = (num: number | string, floorFlg: boolean) => {
     const rate: Rate = num === "00" ? 100 : 10;
-    const type: Type = num === "." ? "/" : "*";
-    num = typeof num === 'string' ? 0 : num;
+    const type: Type = floorFlg ? "/" : "*";
+    const n: number = typeof num === 'string' ? 0 : num;
+    console.log('floorFlg', floorFlg, "type", type);
 
     let current_num = calcs.operator === "" ? calcs.firstNum : calcs.secondNum;
 
-    if (num === 0 && current_num === 0) return;
+    if (n === 0 && current_num === 0) return;
     if (autoFlg) {
       setAutoFlg(false);
       current_num = 0;
     }
-    if (type === "*") current_num = current_num * rate + (num !== 0 ? num : 0);
-    if (type === "/") current_num = current_num / rate + (num !== 0 ? num : 0);
+    if (type === "*") current_num = current_num * rate + (n !== 0 ? n : 0);
+    if (type === "/") {
+      console.log('floorFlg', floorFlg);
+      if ( current_num === 0) {
+        current_num = n / rate;
+      } else {
+        current_num = current_num + (n !== 0 ? n / rate : 0);
+      }
+    }
 
     if (calcs.operator === "") {
       setCalcs({...calcs, firstNum: current_num });
-		} else {
+    } else {
       setCalcs({...calcs, secondNum: current_num });
     }
   };
 
   return (
     <div>
+      {/* リセットボタン */}
       <button onClick={resetHandler}> C </button>
 
+      {/* 数字ボタン */}
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "00", "."].map((num) => {
         return (
           <button key={num}
-            onClick={numBtnHandler.bind(null, num)}
+            onClick={() => {
+              if (num === ".") {
+                setFloorFlg(true);
+                numBtnHandler(num, true);
+              } else {
+                numBtnHandler(num, floorFlg);
+              }
+            }}
           >
             {num}
           </button>
         );
       })}
 
+      {/* 演算子ボタン */}
       {["+", "-", "*", "/"].map((op) => {
         return (
           <button key={op} onClick={ optHandler.bind(null, op as Operator)}>
@@ -83,6 +113,7 @@ function Buttons({
         );
       })}
 
+      {/* =ボタン */}
       <button onClick={calcHandler}> = </button>
     </div>
   );
