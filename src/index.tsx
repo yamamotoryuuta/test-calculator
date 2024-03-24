6d53b9c6339ef3e53f1d08207db85d59d3d5e702
yamamotoryuuta <ryuuta.0914@icloud.com>
2024-03-24 16:40:12 +0900

    スタイル修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/reset.css';
 import './styles/index.css';
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
